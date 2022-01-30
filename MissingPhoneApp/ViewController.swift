15b4ed7b74b315d7e365ceb52958c831f8a47a78
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2022-01-29 23:41:42 -0600

    1st commit

--- MissingPhoneApp/ViewController.swift
+++ MissingPhoneApp/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var labelAddress: UILabel!
+    @IBOutlet var labelName: UILabel!
+    @IBOutlet var buttonMain: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onButtonPress(){
+        buttonMain.setTitle("Info Shown", for: .normal)
+        
+        labelName.text = "Jacob Miller"
+        labelAddress.text = "Grandview Dr. Johnsburg IL"
+    }
 
 }
 
